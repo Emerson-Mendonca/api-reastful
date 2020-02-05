0bf20a3eac54beb7c59819d939c4d7bb005eba08
emerson <emersonsilva321@gmail.com>
2020-02-05 01:12:52 -0300

    Crindo Controle Mapping. =)

--- src/test/java/com/api/rest/steps/DemoApplicationTest.java
+++ src/test/java/com/api/rest/steps/DemoApplicationTest.java
@@ -1,12 +1,19 @@
 package com.api.rest.steps;
 
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.pt.Entao;
 
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DemoApplicationTest {
 	private static final Logger LOG = LoggerFactory.getLogger(DemoApplicationTest.class);
 
@@ -25,7 +32,7 @@ public class DemoApplicationTest {
 	@Entao("que a {string} eo {string} sejam o mesmo.")
 	public void queAEoSejamOMesmo(String string, String string2) {
 		// Write code here that turns the phrase above into concrete actions
-		LOG.info("- OK-1 -");
+		LOG.info("- OK-2 -");
 
 	}
 }
