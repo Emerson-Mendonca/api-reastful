ea60d5f5b99ebe4026b83bf1b942c1e24815d4e2
emerson <emersonsilva321@gmail.com>
2020-02-05 17:33:15 -0300

    Updata Not =)

--- src/main/java/com/api/rest/controller/ExampleController.java
+++ src/main/java/com/api/rest/controller/ExampleController.java
@@ -1,17 +1,26 @@
 package com.api.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/example")
+@RequestMapping("/api")
 public class ExampleController {
 
-    @GetMapping("/hello-world")
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Hello World!");
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleController.class);
+
+    // Map to store employees, ideally we should use database
+
+    @RequestMapping(value = EmpRestURIConstants.GET_EMP, method = RequestMethod.GET)
+    public @ResponseBody Object getEmployee(@PathVariable("id") int empId) {
+        LOG.info("Start getEmployee. ID=" + empId);
+        return ResponseEntity.ok(empId);
     }
-    
+
 }
--- src/test/java/com/api/rest/steps/DemoApplicationTest.java
+++ src/test/java/com/api/rest/steps/DemoApplicationTest.java
@@ -10,29 +10,28 @@ import io.cucumber.java.Before;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.pt.Entao;
 
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTest {
-	private static final Logger LOG = LoggerFactory.getLogger(DemoApplicationTest.class);
 
-	@Before
-	public void setUp() {
-		LOG.info("-------------- Spring Context Initialized For Executing Cucumber Tests --------------");
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplicationTest.class);
 
-	@Dado("que ao buscar {string}")
-	public void queAoBuscar(String string) {
-		// Write code here that turns the phrase above into concrete actions
-		LOG.info("- OK-1 -");
+    @Before
+    public void setUp() {
+        LOG.info("-------------- Spring Context Initialized For Executing Cucumber Tests --------------");
+    }
 
-	}
+    @Dado("que ao buscar {string}")
+    public void queAoBuscar(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        LOG.info("- OK-1 -");
 
-	@Entao("que a {string} eo {string} sejam o mesmo.")
-	public void queAEoSejamOMesmo(String string, String string2) {
-		// Write code here that turns the phrase above into concrete actions
-		LOG.info("- OK-2 -");
+    }
 
-	}
+    @Entao("que a {string} eo {string} sejam o mesmo.")
+    public void queAEoSejamOMesmo(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        LOG.info("- OK-2 -");
+
+    }
 }
